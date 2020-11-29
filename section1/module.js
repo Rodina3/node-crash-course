52ec21f66686b3b460fe930e694db43d755c09b0
Rong <buaazhourong@163.com>
2020-11-29 10:57:46 +0800

    [Rong] import variables from other module

--- section1/module.js
+++ section1/module.js
@@ -1,3 +1,8 @@
+// import variables from other file
 const { people, age } = require('./people');
-
 console.log(people, age);
+
+
+// import variables from other module
+const os = require('os');
+console.log(os.platform(), os.homedir());
