d4d4d5c57a16ab0c5a621c4e7f1af9eead43897e
Rong <buaazhourong@163.com>
2020-12-02 10:13:29 +0800

    [Rong] return index.html in response

--- section3/server.js
+++ section3/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
@@ -6,9 +7,25 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<h1>hello, rong</h1>');
-    res.write('<p>hello again, rong</p>');
-    res.end();
+    // // return html elements
+    // res.write('<h1>hello, rong</h1>');
+    // res.write('<p>hello again, rong</p>');
+    // res.end();
+
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // if no need to write multiple times, can directly pass it to end() function
+            // it does the same as 
+            // res.write(data); 
+            // res.end();
+            res.end(data); 
+        }
+       
+    });
 });
 
 server.listen(3000, 'localhost', () => {
