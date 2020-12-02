2d37f13f969811d291c5a9c011e48073f2b1b439
Rong <buaazhourong@163.com>
2020-12-02 16:34:50 +0800

    [Rong] use middleware

--- section6/app.js
+++ section6/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -11,6 +12,19 @@ app.set('view engine', 'ejs');
 app.listen(3000, () => {
     console.log("server started, listen to port 3000");
 });
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log("in the next middleware");
+    next();
+})
 
 app.get('/', (req, res) => {
     const blogs = [
