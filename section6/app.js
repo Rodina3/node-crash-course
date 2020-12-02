6f7c9a696d76da5167461be82ce2e8799a4e20d0
Rong <buaazhourong@163.com>
2020-12-02 17:02:57 +0800

    [Rong] use 3rd party middleware

--- section6/app.js
+++ section6/app.js
@@ -13,18 +13,9 @@ app.listen(3000, () => {
     console.log("server started, listen to port 3000");
 });
 
-app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();
-})
 
-app.use((req, res, next) => {
-    console.log("in the next middleware");
-    next();
-})
+// middleware & static file
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
